7f9d6a19e9ff1292ba45675ab65c09280206329c
Shashank Singh <62293640+ssrajputtheboss@users.noreply.github.com>
2021-08-22 20:01:14 +0530

    removed expandable profile link replaced with normal icon link

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Flex } from "@chakra-ui/layout";
-import { ProfileLinkExpandable } from "./ProfileLinkExpandable";
 import { 
     SiGithub, 
     SiTwitter ,
-    SiLinkedin
+    SiLinkedin ,
+    SiDocker
 } from "react-icons/si";
+import Icon from "@chakra-ui/icon";
 
 export const Footer = ():React.ReactElement =>{
-    const profiles = [
+    /*const profiles = [
         'https://github.com/ssrajputtheboss',
         'http://twitter.com/ssrajputtheboss',
     ];
@@ -21,17 +22,29 @@ export const Footer = ():React.ReactElement =>{
         'white',
         'twitter.500',
         'blue.800'
-    ]
+    ]*/
     return <footer style={{ height:'fit-content', width:'100vw' }}>
         <Flex
         flexDir="row"
         justifyContent="space-around"
         alignItems="center"
         w="full"
-        h="52"
+        h="32"
         bgColor="black"
         >
-            {profiles.map((url:string,i:number)=><ProfileLinkExpandable color={colors[i]} icon={icons[i]} url={url} />)}
+            <a href="https://github.com/ssrajputtheboss">
+                <Icon as={SiGithub} color="white" />
+            </a>
+            <a href="http://twitter.com/ssrajputtheboss">
+                <Icon as={SiTwitter} color="twitter.500" />
+            </a>
+            <a href="https://www.linkedin.com/in/shashank-singh-0aaab8208/">
+                <Icon as={SiLinkedin} color="blue.800" />
+            </a>
+            <a href="https://hub.docker.com/u/ssrajputtheboss">
+                <Icon as={SiDocker} color="blue.500" />
+            </a>
         </Flex>
     </footer>
 }
+//             {profiles.map((url:string,i:number)=><ProfileLinkExpandable color={colors[i]} icon={icons[i]} url={url} />)}
